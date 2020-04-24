e1393408729def480986044c52cc220453a13f89
Caroline Nunes <carolinenunes@Carolines-MacBook-Pro.local>
2020-04-24 16:44:57 +1000

    Course Test Spring Boot- privacy

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -9,14 +9,14 @@ public class MoneyTest {
     public void shouldReturnTenGivenFive(){
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
-        assertEquals(10, product.amount);
+        assertEquals(new Dollar(10), product);
     }
 
     @Test
     public void shouldReturnFifteenGivenFive(){
         Dollar five = new Dollar(5);
         Dollar product = five.times(3);
-        assertEquals(15, product.amount);
+        assertEquals(new Dollar(15), product);
     }
 
     @Test
