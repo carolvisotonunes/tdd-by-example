18cc474d2cf33f5ccfd36f1b05fde2f110ab1631
Caroline Nunes <carolinenunes@Carolines-MacBook-Pro.local>
2020-04-24 16:55:52 +1000

    Course Test Spring Boot- fancySpeaking

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -6,26 +6,52 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 public class MoneyTest {
 
     @Test
-    public void shouldReturnTenGivenFive(){
+    public void shouldReturnTenGivenFiveDollar(){
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
         assertEquals(new Dollar(10), product);
     }
 
     @Test
-    public void shouldReturnFifteenGivenFive(){
+    public void shouldReturnFifteenGivenFiveDollar(){
         Dollar five = new Dollar(5);
         Dollar product = five.times(3);
         assertEquals(new Dollar(15), product);
     }
 
     @Test
-    public void shouldReturnTrueGivenEqualObjects(){
+    public void shouldReturnTrueGivenEqualObjectsDollar(){
         assertEquals(new Dollar(5), new Dollar(5));
     }
 
     @Test
-    public void shouldReturnFalseGivenDifferentObjects(){
+    public void shouldReturnFalseGivenDifferentObjectsDollar(){
         assertNotEquals(new Dollar(5), new Dollar(3));
+    }
+
+
+
+    @Test
+    public void shouldReturnTenGivenFiveFranc(){
+        Franc five = new Franc(5);
+        Franc product = five.times(2);
+        assertEquals(new Franc(10), product);
+    }
+
+    @Test
+    public void shouldReturnFifteenGivenFiveFranc(){
+        Franc five = new Franc(5);
+        Franc product = five.times(3);
+        assertEquals(new Franc(15), product);
+    }
+
+    @Test
+    public void shouldReturnTrueGivenEqualObjectsFranc(){
+        assertEquals(new Franc(5), new Franc(5));
+    }
+
+    @Test
+    public void shouldReturnFalseGivenDifferentObjectsFranc(){
+        assertNotEquals(new Franc(5), new Franc(3));
     }
 }
