5c7d58f17ecaa876256724ff199c5c8150f1df55
Caroline Nunes <carolinenunes@Carolines-MacBook-Pro.local>
2020-05-01 17:25:53 +1000

    Course Test Spring Boot- additionExpressionBank

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,4 +1,4 @@
-public  class Money {
+public  class Money implements Expression {
     protected int amount;
     protected String currency;
 
@@ -34,5 +34,9 @@ public  class Money {
     public Money times(int multiplier) {
         return new Money(amount *= multiplier, this.currency);
     }
+    public Expression plus(Money addend){
+        return new Money( amount +addend.amount, currency);
+    }
+
 
 }
